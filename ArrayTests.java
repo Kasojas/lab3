ec4649f5a1fe9eb4df3a7c4fd6ce60fee0581993
Kasojas <156245225+Kasojas@users.noreply.github.com>
2024-01-31 12:44:05 -0800

    new

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,30 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReveresedInPlaceFullArray() {
+    int[] input = {1, 2 ,3 ,4};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{4, 3, 2, 1}, input);
+  }
+
+  @Test
+  public void testReversedNoChange() {
+    int[] input = {4, 3, 3, 4};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{4, 3, 3, 4}, input);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedFullArray() {
+    int[] input = {1, 2, 3, 4};
+    assertArrayEquals(new int[]{4, 3, 2, 1}, ArrayExamples.reversed(input));
+    assertArrayEquals(new int[] {1, 2, 3, 4}, input);
   }
 }
